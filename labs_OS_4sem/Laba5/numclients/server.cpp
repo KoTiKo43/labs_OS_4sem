5dfbb0e6d0383dd503841e5d0356685d6c8aad4b
KoTiKo43 <akimnik2005@mail.ru>
2025-04-15 16:16:44 +0400

    Лр5: Типа разделение

---
+++ labs_OS_4sem/Laba5/numclients/server.cpp
@@ -0,0 +1,199 @@
+#include <iostream>
+#include <WinSock2.h>
+#include <vector>
+#include <limits>
+#include <ios>
+#include <string>
+#include <map>
+#include <sstream>
+using namespace std;
+
+// Вектор сокетов
+vector<SOCKET> Connections;
+
+// Создание семафора
+HANDLE hSemaphore;
+
+char decodeMorse(const string &ch)
+{
+    static map<string, char> morseDecode = {
+        {".-", 'A'}, {"-...", 'B'}, {"-.-.", 'C'}, {"-..", 'D'}, {".", 'E'}, 
+        {"..-.", 'F'}, {"--.", 'G'}, {"....", 'H'}, {"..", 'I'}, {".---", 'J'}, 
+        {"-.-", 'K'}, {".-..", 'L'}, {"--", 'M'}, {"-.", 'N'}, {"---", 'O'}, 
+        {".--.", 'P'}, {"--.-", 'Q'}, {".-.", 'R'}, {"...", 'S'}, {"-", 'T'}, 
+        {"..-", 'U'}, {"...-", 'V'}, {".--", 'W'}, {"-..-", 'X'}, {"-.--", 'Y'}, 
+        {"--..", 'Z'}, {".----", '1'}, {"..---", '2'}, {"...--", '3'}, {"....-", '4'}, 
+        {".....", '5'}, {"-....", '6'}, {"--...", '7'}, {"---..", '8'}, {"----.", '9'}, 
+        {"-----", '0'}
+    };
+
+    if (morseDecode.find(ch) != morseDecode.end())
+    {
+        return morseDecode[ch];
+    }
+    else
+    {
+        return '?';
+    }
+}
+
+string decodeMorseMsg(const char *msg)
+{
+    stringstream ss(msg);
+    string ch;
+    string result = "";
+    while (ss >> ch)
+    {
+        if (ch == "/")
+        {
+            result += "";
+        }
+        else if (ch == "#")
+        {
+            result += " ";
+        }
+        else
+        {
+            result += decodeMorse(ch);
+        }
+    }
+    return result;
+}
+
+void ClientHandler(int index)
+{
+    while (true)
+    {
+        // Ожидание семафора
+        WaitForSingleObject(hSemaphore, INFINITE);
+
+        int msg_size;
+
+        // Обработка отключения клиента
+        if (recv(Connections[index], (char *)&msg_size, sizeof(int), NULL) <= 0)
+        {
+            cout << "Юстас №" << index + 1 << " прервал связь" << endl;
+            closesocket(Connections[index]);
+            ReleaseSemaphore(hSemaphore, 1, NULL);
+            break;
+        }
+
+        char *msg = new char[msg_size + 1];
+        msg[msg_size] = '\0';
+        recv(Connections[index], msg, msg_size, NULL);
+
+        if (string(msg) == "_END")
+        {
+            cout << "Юстас №" << index + 1 << " прервал связь" << endl;
+            closesocket(Connections[index]);
+            delete[] msg;
+            ReleaseSemaphore(hSemaphore, 1, NULL);
+            break;
+        }
+
+        cout << "Шифровка от Юстаса №" << index + 1 << ": " << msg << endl;
+        cout << "Дешифровка: " << decodeMorseMsg(msg) << endl;
+
+        delete[] msg;
+
+        // Освобождение семафора
+        ReleaseSemaphore(hSemaphore, 1, NULL);
+    }
+}
+
+void SafeInputNumClients(int &value, const string &prompt)
+{
+    while (true)
+    {
+        cout << prompt;
+        cin >> value;
+
+        if (cin.fail())
+        {
+            cin.clear();
+            cin.ignore((std::numeric_limits<std::streamsize>::max)(), '\n');
+            cerr << "Ошибка ввода! Введите число" << endl;
+            continue;
+        }
+
+        if (value < 1)
+        {
+            cerr << "Значение должно быть >= 1" << endl;
+            continue;
+        }
+        break;
+    }
+    cin.ignore((std::numeric_limits<std::streamsize>::max)(), '\n');
+}
+
+int main()
+{
+    // Установка кодировки вывода консоли
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+
+    int numClients;
+    SafeInputNumClients(numClients, "Введите количество пользователей: ");
+    Connections.resize(numClients);
+
+    // WSAStartup
+    WSADATA wsaData;
+    WORD DLLVersion = MAKEWORD(2, 1);          // Запрашиваемая версия библиотеки WinSock
+    if (WSAStartup(DLLVersion, &wsaData) != 0) // Загрузка библиотеки
+    {
+        cerr << "Ошибка WSAStartup" << endl;
+        return 1;
+    }
+
+    SOCKADDR_IN addr; // Хранение адреса для интернет-протокола
+    int sizeofaddr = sizeof(addr);
+    addr.sin_addr.s_addr = inet_addr("127.0.0.1"); // sin_addr хранит ip-адрес (здесь - localhost)
+    addr.sin_port = htons(1111);                   // Установка порта
+    addr.sin_family = AF_INET;                     // Семейство протоколов (константа AF_INET для интернет-протоколов)
+
+    SOCKET sListen = socket(AF_INET, SOCK_STREAM, NULL); // Создание сокета для прослушивания
+    bind(sListen, (SOCKADDR *)&addr, sizeof(addr));      // Привязка адреса сокету
+    listen(sListen, SOMAXCONN);                          // Запуск прослушивания
+
+    // Создание семафора при n = 2
+    int n = 2;
+    hSemaphore = CreateSemaphore(NULL, n, n, NULL);
+
+    SOCKET newConnection; // Сокет для удержания соединения с клиентом
+    cout << "Связь настроена. Ожидание подключений..." << endl;
+
+    for (int i = 0; i < numClients; i++)
+    {
+        newConnection = accept(sListen, (SOCKADDR *)&addr, &sizeofaddr);
+
+        // Проверка подключения клиента к серверу
+        if (newConnection == 0)
+        {
+            cerr << "Ошибка, клиент не может подключиться к серверу" << endl;
+        }
+        else
+        {
+            Connections.push_back(newConnection);
+            int clientNumber = Connections.size(); // Номер клиента
+            cout << "Юстас №" << clientNumber << " на связи!" << endl;
+
+            // Отправляем номер клиенту
+            send(newConnection, (char *)&clientNumber, sizeof(int), NULL);
+
+            // Запускаем поток для обработки клиента
+            CreateThread(NULL, NULL, (LPTHREAD_START_ROUTINE)ClientHandler, (LPVOID)(Connections.size() - 1), NULL, NULL);
+        }
+    }
+
+    // Закрытие семафора
+    CloseHandle(hSemaphore);
+
+    for (SOCKET sock : Connections)
+    {
+        closesocket(sock);
+    }
+    closesocket(sListen);
+    WSACleanup();
+    system("pause");
+    return 0;
+}
---
+++ labs_OS_4sem/Laba5/whiletrue/client.cpp
@@ -0,0 +1,107 @@
+#include <iostream>
+#include <WinSock2.h>
+#include <string>
+#include <map>
+using namespace std;
+
+SOCKET Connection;
+
+string encodeToMorse(const string &text)
+{
+    static map<char, string> morseCode = {
+        {'A', ".-"}, {'B', "-..."}, {'C', "-.-."}, {'D', "-.."}, {'E', "."},
+        {'F', "..-."}, {'G', "--."}, {'H', "...."}, {'I', ".."}, {'J', ".---"},
+        {'K', "-.-"}, {'L', ".-.."}, {'M', "--"}, {'N', "-."}, {'O', "---"},
+        {'P', ".--."}, {'Q', "--.-"}, {'R', ".-."}, {'S', "..."}, {'T', "-"},
+        {'U', "..-"}, {'V', "...-"}, {'W', ".--"}, {'X', "-..-"}, {'Y', "-.--"},
+        {'Z', "--.."}, {'1', ".----"}, {'2', "..---"}, {'3', "...--"}, {'4', "....-"}, {'5', "....."},
+        {'6', "-...."}, {'7', "--..."}, {'8', "---.."}, {'9', "----."}, {'0', "-----"},
+    };
+    
+    string result;
+    for (char c : text)
+    {
+        if (c == ' ')
+        {
+            result += "# "; // Пробел между словами
+        }
+        else
+        {
+            char upperC = toupper(c);
+            if (morseCode.find(upperC) != morseCode.end())
+            {
+                result += morseCode[upperC] + " ";
+            }
+            else
+            {
+                result += "? "; // Неизвестный символ
+            }
+        }
+    }
+    return result;
+}
+
+int main()
+{
+    // Установка кодировки вывода консоли
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+
+    // WSAStartup
+    WSADATA wsaData;
+    WORD DLLVersion = MAKEWORD(2, 1);
+    if (WSAStartup(DLLVersion, &wsaData) != 0)
+    {
+        cerr << "Ошибка WSAStartup" << endl;
+        return 1;
+    }
+
+    SOCKADDR_IN addr;
+    int sizeofaddr = sizeof(addr);
+    addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    addr.sin_port = htons(1111);
+    addr.sin_family = AF_INET;
+
+    Connection = socket(AF_INET, SOCK_STREAM, 0); // Сокет для соединения с сервером
+    // Попытка соединения с сервером
+    if (connect(Connection, (SOCKADDR *)&addr, sizeof(addr)) != 0)
+    {
+        cerr << "Ошибка, не удалось подключиться к серверу" << endl;
+        return 1;
+    }
+    // Получение номера от сервера
+    int clientNumber;
+    recv(Connection, (char *)&clientNumber, sizeof(int), NULL);
+
+    cout << "Связь установлена. Здравствуйте, Юстас №" << clientNumber << endl;
+    cout << "Введите сообщение (или '_END' для завершения сеанса):" << endl;
+
+    // Отправка строки серверу
+    string msg;
+    while (true)
+    {
+        getline(cin, msg);
+        if (msg == "_END") {
+            // Отправляем сигнал конца сеанса
+            int msg_size = msg.size();
+            send(Connection, (char *)&msg_size, sizeof(int), NULL);
+            send(Connection, msg.c_str(), msg_size, NULL);
+            break;
+        }
+
+        // Кодируем сообщение в азбуку Морзе
+        string morseMsg = encodeToMorse(msg);
+        cout << "Отправляемая шифровка: " << morseMsg << endl;
+
+        // Отправляем закодированное сообщение
+        int msg_size = morseMsg.size();
+        send(Connection, (char *)&msg_size, sizeof(int), NULL);
+        send(Connection, morseMsg.c_str(), msg_size, NULL);
+        Sleep(10);
+    }
+
+    closesocket(Connection);
+    WSACleanup();
+    system("pause");
+    return 0;
+}
